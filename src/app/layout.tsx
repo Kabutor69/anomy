19e99a56a558d0b8b3053ad5b2cdc0b1833f29a7
Kabutor69 <pisionz69@gmail.com>
2025-11-18 21:16:01 +0545

    Enhance metadata for OpenGraph and Twitter
    Updated the metadata description and added OpenGraph and Twitter metadata for better social sharing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,33 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Anomy",
-  description: "No faces, no filters, no names. Just real, honest voices sharing their true thoughts and feelings openly, without fear or judgment. A safe place where your words matter more than who you are.",
+  description:
+    "No faces, no filters, no names. Just real, honest voices sharing their true thoughts and feelings openly, without fear or judgment. A safe place where your words matter more than who you are.",
   verification: {
     google: "Q2d_2g4XEV5pIiAAxAEDIvhDvj6-4BihRRx9EcNKkDk",
+  },
+  openGraph: {
+    title: "Anomy",
+    description:
+      "No faces, no filters, no names. Just real, honest voices sharing their true thoughts and feelings openly.",
+    url: "https://anomy-teal.vercel.app",
+    siteName: "Anomy",
+    images: [
+      {
+        url: "https://anomy-teal.vercel.app/favicon.ico",
+        width: 64,
+        height: 64,
+        alt: "Anomy Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Anomy",
+    description:
+      "No faces, no filters, no names. Just real, honest voices sharing their true thoughts and feelings openly.",
+    images: ["https://anomy-teal.vercel.app/favicon.ico"], 
   },
 };
 
